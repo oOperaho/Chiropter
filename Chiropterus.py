7f87bfe2158a1f7100bcdbe9f3551848e22f864e
Operaho <emptyloperaho@gmail.com>
2022-03-01 11:52:11 -0300

    first commit

---
+++ Chiropterus.py
@@ -0,0 +1,33 @@
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+objects = ["Menu", "TxtLayout"]
+
+class TextEditor(QWidget):
+
+	def __init__(self):
+		QWidget.__init__(self)
+		self.setGeometry(100, 100, 300, 400)
+		self.setObjectName(objects[0])
+		self.current_path = None
+		self.txt_field = QPlainTextEdit(self)
+		self.txt_field.setObjectName(objects[1])
+
+
+
+
+def display():
+	import sys
+	w = QApplication(sys.argv)
+	w.setApplicationName("Chiropter")
+	with open("BatStyle.qss", "r") as f:
+		qss = f.read()
+		w.setStyleSheet(qss)
+	ui = TextEditor()
+	ui.show()
+	sys.exit(w.exec())
+
+
+display()
