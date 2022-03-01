aa29bdc0bfe3d1f9a29c9c4eca28519c20621ec9
Operaho <emptyloperaho@gmail.com>
2022-03-01 19:55:10 -0300

    text field added

--- Chiropterus.py
+++ Chiropterus.py
@@ -3,7 +3,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
-objects = ["Menu", "TxtLayout"]
+objects = ["Menu", "LineEdit"]
 
 class TextEditor(QWidget):
 
@@ -12,15 +12,12 @@ class TextEditor(QWidget):
 		self.setGeometry(300, 300, 500, 500)
 		self.setMinimumSize(200, 200)
 		self.setObjectName(objects[0])
+		self.txt_editor = QPlainTextEdit(self)
 		self.current_path = None
 		self.mainui()
 
 	def mainui(self):
-		self.layout = QVBoxLayout(self)
-		self.txt_editor = QPlainTextEdit(self)
-
-		self.layout.addWidget(self.txt_editor)
-		self.layout.setObjectName(objects[1])
+		self.txt_editor.setObjectName(objects[1])
 
 
 
@@ -29,9 +26,11 @@ def display():
 	import sys
 	w = QApplication(sys.argv)
 	w.setApplicationName("Chiropter")
-	with open("BatStyle.qss", "r") as f:
-		qss = f.read()
+	BatStyle = open("BatStyle.qss", "r")
+	with BatStyle:
+		qss = BatStyle.read()
 		w.setStyleSheet(qss)
+	w.setStyle("Breeze")
 	ui = TextEditor()
 	ui.show()
 	sys.exit(w.exec())
