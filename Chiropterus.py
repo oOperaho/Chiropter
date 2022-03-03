797c92082c240e7147e8425510e0baaddfbcda6d
Operaho <emptyloperaho@gmail.com>
2022-03-03 10:34:09 -0300

    add actions

--- Chiropterus.py
+++ Chiropterus.py
@@ -5,6 +5,7 @@ from PyQt5.QtCore import *
 
 objects = ["Menu", "LineEdit", "Box", "MenuBar"]
 
+
 class TextEditor(QMainWindow):
 	def __init__(self, parent=None):
 		super().__init__(parent)
@@ -13,25 +14,27 @@ class TextEditor(QMainWindow):
 		self.txt_editor = QPlainTextEdit(self)
 		self.txt_layout = QVBoxLayout(self)
 		self.txt_widget = QWidget(self)
+		self.open_file = QAction("&New", self)
 		self.menu = QMenuBar(self)
 		self.setMenuBar(self.menu)
 		self.txtField()
-		self._createMenuBar()
+		self.MenuBar()
 
-
-	def _createMenuBar(self):
+	def MenuBar(self):
 		self.menu.addMenu("&File")
 		self.menu.addMenu("&Edit")
 		self.menu.addMenu("&Style")
 		self.menu.setObjectName(objects[3])
+
+
 
 	def txtField(self):
 		self.txt_editor.setObjectName(objects[1])
 		self.txt_layout.setObjectName(objects[2])
 		self.txt_layout.addWidget(self.txt_editor)
 		self.txt_widget.setLayout(self.txt_layout)
+		self.setCentralWidget(self.txt_widget)
 		
-
 
 def display():
 	import sys
